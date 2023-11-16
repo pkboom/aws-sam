a3418916557e695d36b607478f010be995f74c63
Keunbae Park <pkboom@users.noreply.github.com>
2023-11-16 15:51:52 -0500

    wip

--- sqs-lambda/src/app.mjs
+++ sqs-lambda/src/app.mjs
@@ -1,8 +1,24 @@
-export const lambdaHandler = async event => {
+import { GetObjectCommand, S3Client } from '@aws-sdk/client-s3'
+
+const client = new S3Client({})
+
+export const handler = async event => {
   try {
     console.log(event)
 
-    console.log(event.Records[0].body)
+    const command = new GetObjectCommand({
+      Bucket: 'sqs-lambda-sqslambdas3-1cmn665rqy0hj',
+      Key: 'test.txt',
+    })
+
+    try {
+      const response = await client.send(command)
+      // The Body object also has 'transformToByteArray' and 'transformToWebStream' methods.
+      const str = await response.Body.transformToString()
+      console.log(str)
+    } catch (err) {
+      console.error(err)
+    }
 
     return {
       statusCode: 200,
