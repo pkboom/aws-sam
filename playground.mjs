c57fd3c53eabca20bbe1bc5856f69967b761f7a2
Keunbae Park <pkboom@users.noreply.github.com>
2023-11-18 19:22:38 -0500

    wip

--- playground.mjs
+++ playground.mjs
@@ -1,68 +1,11 @@
-import { readFileSync } from 'fs'
-import path, { resolve } from 'path'
-import { fileURLToPath } from 'url'
-import { XMLParser } from 'fast-xml-parser'
-import dns from 'dns'
-
-let __dirname = path.dirname(fileURLToPath(import.meta.url))
-
 const run = async () => {
-  let xml = readFileSync(path.join(__dirname, 'data', 'google.com!spotify.com!1693958400!1694044799.xml'), 'utf-8')
+  let promise = new Promise(function (resolve) {
+    setTimeout(() => resolve('done'), 3000)
+  })
 
-  let json = new XMLParser().parse(xml)
-
-  let records = Array.isArray(json.feedback.record) ? json.feedback.record : [json.feedback.record]
-
-  let ips = []
-
-  for (let i = 0; i < records.length; i++) {
-    ips.push(records[i].row.source_ip)
-  }
-
-  console.log(ips.length)
-
-  console.log(records.length)
-
-  let uniqueIps = ips.filter((value, index, self) => self.indexOf(value) === index)
-
-  console.log(uniqueIps.length)
-
-  let reverses = []
-
-  for (let i = 0; i < uniqueIps.length; i++) {
-    let ip = uniqueIps[i]
-
-    reverses.push(reverseLookup(ip))
-  }
-
-  let result = await Promise.all(reverses)
+  let result = [1, await promise] // wait till the promise resolves (*)
 
   console.log(result)
-  console.log(result.length)
-
-  console.log(records.length)
-
-  console.log(ips.length)
-
-  console.log(uniqueIps.length)
-
-  let reverse = await dns.promises.reverse('216.58.194.142')
-
-  console.log(reverse)
 }
 
 run()
-
-async function reverseLookup(ip) {
-  try {
-    let reverse = dns.promises.reverse(ip)
-
-    let timeout = new Promise(resolve => {
-      setTimeout(resolve, 5000, [ip, [null]])
-    })
-
-    return [ip, await Promise.race([reverse, timeout])]
-  } catch (error) {
-    return [ip, [null]]
-  }
-}
