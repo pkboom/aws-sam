bb526ed6027a34bd53e7cab7171fbd565e8fa194
Keunbae Park <pkboom@users.noreply.github.com>
2023-12-05 10:49:44 -0500

    wip

---
+++ lambda/README.md
@@ -0,0 +1,3 @@
+# [ElastiCache Node Types](https://www.dragonflydb.io/guides/elasticache-node-instance-types)
+
+# [Supported node types](https://docs.aws.amazon.com/AmazonElastiCache/latest/red-ug/CacheNodes.SupportedTypes.html#CacheNodes.SupportedTypesByRegion)
---
+++ lambda/layer/helper.js
@@ -0,0 +1,22 @@
+export const currentTimestamp = () => {
+  return new Date().getTime()
+}
+
+function wait(milliseconds) {
+  return new Promise(resolve => setTimeout(resolve, milliseconds))
+}
+
+export { wait }
+
+export const errorMessages = {
+  badXml: 'Bad xml',
+  badAttachment: 'Bad attachment',
+  noAttachment: 'No attachment',
+  badContentType: 'Bad content type',
+  badKey: 'Bad key',
+  test: 'test',
+}
+
+export const REVERSE_LOOKUP = 'reverse_lookup'
+
+export const REVERSE_LOOKUP_INDEX = 'reverse_lookup_index'
