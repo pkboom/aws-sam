259fed86eaa54cb257206c576cb7f6006b9a7f34
Keunbae Park <pkboom@users.noreply.github.com>
2023-12-09 16:34:09 -0500

    wip

--- sam_init.sh
+++ sam_init.sh
@@ -13,8 +13,6 @@ APP_NAME=$1
 
 sam init --runtime nodejs18.x --name $APP_NAME --no-interactive --dependency-manager npm --app-template hello-world
 
-DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+cd $APP_NAME
 
-cd $DIR
-
-find ./ -type f -exec sed -i -e 's/lambdaHandler/handler/g' {} \;
+# find ./ -type f -exec sed -i -e 's/lambdaHandler/handler/g' {} \;
